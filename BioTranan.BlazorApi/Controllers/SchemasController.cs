38f3877126b6637835eef0c2570b33612c14b076
Elin Nyman <elin.nyman87@gmail.com>
2023-04-15 19:25:31 +0200

    Controllers in blazorapi

--- BioTranan.BlazorApi/Controllers/SchemasController.cs
+++ BioTranan.BlazorApi/Controllers/SchemasController.cs
@@ -4,15 +4,15 @@ using BioTranan.Core.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using BioTranan.Core.ViewModels;
 
-namespace BioTranan.Api.Controllers;
+namespace BioTranan.BlazorApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class MovieDetailsController : ControllerBase
+public class SchemasController : ControllerBase
 {
     private readonly IMovieDetailsRepository _webRepository;
 
-    public MovieDetailsController(IMovieDetailsRepository webRepository)
+    public SchemasController(IMovieDetailsRepository webRepository)
     {
         _webRepository = webRepository;
     }
@@ -34,30 +34,6 @@ public class MovieDetailsController : ControllerBase
 
             var schemas = shows.ConvertToDto(movies, salons, bookings);
             return Ok(schemas);
-        }
-        catch (System.Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, "Fel när data försökte hämtas från databasen");
-        }
-    }
-
-    [HttpGet("{id:int}")]
-    public async Task<ActionResult<IEnumerable<ShowDetailsDto>>> GetMovieDetails(int id)
-    {
-        try
-        { // Gus! I have seen this in a tutorial! How can I minimize my try block?
-            var show = await _webRepository.GetShow(id);
-            var movie = await _webRepository.GetMovie(show.MovieId);
-            var salon = await _webRepository.GetSalon(show.SalonId);
-            var bookings = await _webRepository.GetBookings(show.Id);
-
-            if (movie == null || salon == null || show == null)
-            {
-                return NotFound();
-            }
-
-            var movieDetailsDto = show.ConvertToDto(movie, salon, bookings);
-            return Ok(movieDetailsDto);
         }
         catch (System.Exception)
         {
