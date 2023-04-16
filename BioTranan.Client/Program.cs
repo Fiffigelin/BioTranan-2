d469a4214fb1b2954bb24d0a2e811c8acda34f08
Elin Nyman <elin.nyman87@gmail.com>
2023-04-16 12:58:12 +0200

    Remove things from program.cs Blazor

--- BioTranan.Client/Program.cs
+++ BioTranan.Client/Program.cs
@@ -7,18 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5133") });
 builder.Services.AddScoped<IShowsService, ShowsService>();
 builder.Services.AddScoped<IBookingService, BookingService>();
-// Add services to the container.
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -28,7 +26,6 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
