6c95b776c7dc8ddf9fa3e991db2420a843f8ea86
Elin Nyman <elin.nyman87@gmail.com>
2023-04-17 08:12:05 +0200

    SalonExtension minor changes

--- BioTranan.Core/Entities/SalonExtension.cs
+++ BioTranan.Core/Entities/SalonExtension.cs
@@ -4,5 +4,7 @@ public class SalonExtensions
 {
     public int Id { get; set; }
     public int AvailableSeats { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
     public bool IsClosed { get; set; } = false;
 }
